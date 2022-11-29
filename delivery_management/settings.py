189203e595b046b520b27ea2b67ceab2d086c2fa
alphaibrahima <alphaibrahimas95@gmail.com>
2022-11-29 12:18:36 -0800

    Update settings.py

--- delivery_management/settings.py
+++ delivery_management/settings.py
@@ -9,6 +9,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!import os
+from pathlib import Path
+from decouple import config
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-mzx1sp2&9dd!m9cqpdxy-=bhs-0ut+h-wnz-wc6s(u-^2(5$^t"
 
@@ -144,8 +155,144 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 #gmail_send/settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alphaibrahimas95@gmail.com'
-EMAIL_HOST_PASSWORD = 'vueceqqxqyilsate' #past the key or password app here
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' #past the key or password app here
+SECRET_KEY = "django-insecure-mzx1sp2&9dd!m9cqpdxy-=bhs-0ut+h-wnz-wc6s(u-^2(5$^t"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+ALLOWED_HOSTS = ["africarelais.herokuapp.com", "localhost", "127.0.0.1"]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "xlrd",
+    "import_export",
+    "notifications",
+    "social_django",
+    # Login via Google as an exemple, you can choose facebook, twitter as you like
+    "allauth.socialaccount.providers.google",
+    # custom app
+    "user.apps.UserConfig",
+    "page.apps.PageConfig",
+    "delivery.apps.DeliveryConfig",
+    "dash.apps.DashboardConfig",
+    "order.apps.OrderConfig",
+    "coupons.apps.CouponsConfig",
+    "payment.apps.PaymentConfig",
+
+]
+SITE_ID = 1
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social_django.middleware.SocialAuthExceptionMiddleware",
+]
+
+ROOT_URLCONF = "delivery_management.urls"
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "dashboard:dashboard"
+LOGOUT_URL = "logout"
+LOGOUT_REDIRECT_URL = "page:home"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "delivery_management" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "delivery_management.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    "social_core.backends.open_id.OpenIdAuth",
+    "social_core.backends.google.GoogleOAuth2",
+    "social_core.backends.facebook.FacebookOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+#gmail_send/settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
